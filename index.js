4078ac80c7a34ff9f75dad0ea994422e8877a6da
Al Vazquez <al.vazquez@gmail.com>
2015-08-01 00:21:40 -0400

    Move hardcoded paths to global variables

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function FixMe() { }
 // Strings to scan for in source
 var fixmeStrings = ["FIXME", "TODO", "HACK", "XXX", "BUG"];
 
+var sourcePathPrefix = "/code/";
+var configJSONPath = "/config.json";
+
 // Prints properly structured Issue data to STDOUT according to
 // Code Climate Engine specification.
 var printIssue = function(fileName, lineNum, matchedString){
@@ -47,7 +50,7 @@ var findFixmes = function(file){
         var cols = line.split(":");
 
         // Remove remnants of container paths for external display
-        var fileName = cols[0].split("/code/")[1];
+        var fileName = cols[0].split(sourcePathPrefix)[1];
         var lineNum = cols[1];
         var matchedString = cols[2];
 
@@ -63,10 +66,10 @@ var findFixmes = function(file){
 // excluding files passed in with by CLI config
 var fileWalk = function(excludePaths){
   var analysisFiles = [];
-  var allFiles = glob.sync("/code/**/**", {});
+  var allFiles = glob.sync(sourcePathPrefix+"**/**", {});
 
   allFiles.forEach(function(file, i, a){
-    if(excludePaths.indexOf(file.split("/code/")[1]) < 0) {
+    if(excludePaths.indexOf(file.split(sourcePathPrefix)[1]) < 0) {
       if(!fs.lstatSync(file).isDirectory()){
         analysisFiles.push(file);
       }
@@ -79,8 +82,8 @@ var fileWalk = function(excludePaths){
 FixMe.prototype.runEngine = function(){
 
   // Check for existence of config.json, parse exclude paths if it exists
-  if (fs.existsSync("/config.json")) {
-    var engineConfig = JSON.parse(fs.readFileSync("/config.json"));
+  if (fs.existsSync(configJSONPath)) {
+    var engineConfig = JSON.parse(fs.readFileSync(configJSONPath));
     var excludePaths = engineConfig.exclude_paths;
   } else {
     var excludePaths = [];
