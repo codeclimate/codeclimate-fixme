1c68053e6f1e9871d336593c56072f14a9cfc214
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2016-02-04 14:59:07 -0500

    Fix bug in how we determine file name
    Don't take first element after splitting on '/code/', because it creates a
    bug when `/code` is included in source code file path.
    
    Add spec.

--- test/fix-me.js
+++ test/fix-me.js
@@ -1,5 +1,7 @@
-var expect = require('chai').expect
-var fixMe = require('../lib/fix-me.js')
+var expect = require('chai').expect,
+    intercept = require("intercept-stdout"),
+    fixMe = require('../lib/fix-me.js'),
+    engine = new fixMe;
 
 describe("fixMe", function(){
   describe("#runEngine()", function(){
@@ -7,9 +9,32 @@ describe("fixMe", function(){
       // expect();
     });
   });
-  describe("#find(file)", function(){
-    xit("finds target fixme keywords", function(){
-      // expect();
+
+  describe('#find(file)', function(){
+    it('finds and correctly prints TODO issues', function(done){
+      var capturedText = "",
+          unhookIntercept = intercept(function(txt) {
+            capturedText += txt;
+          });
+
+      engine.find('test/fixtures/code/src/code/test.js', ["FIXME", "TODO", "HACK", "XXX", "BUG"]);
+
+      // to capture standard output from engine, wait.
+      setTimeout(function() {
+          unhookIntercept();
+
+          expect(capturedText).to.eq('{"type":"issue","check_name":"TODO","description":"TODO found","categories":["Bug Risk"],"location":{"path":"test/fixtures/code/src/code/test.js","lines":{"begin":5,"end":5}}}\0\n');
+          done();
+      }, 10);
+    });
+  });
+
+  describe('#formatPath(path)', function(){
+    it('returns correct filename for files with /code in them', function(){
+      var path = '/code/src/javascripts/code/test.js',
+          formatted = engine.formatPath(path);
+
+      expect(formatted).to.eq('src/javascripts/code/test.js');
     });
   });
 });
